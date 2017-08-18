e684bfd774bd4793ed004a12eaccaf0efef9c04e
breezefaith <nyzhangzc@qq.com>
2017-08-18 18:15:10 +0800

    1111

--- src/AB无算术运算符求和问题/Solution.java
+++ src/AB无算术运算符求和问题/Solution.java
@@ -3,17 +3,29 @@ package AB无算术运算符求和问题;
 public class Solution {
 
 	public static void main(String[] args) {
-		int a=-1;
-		int b=-2;
+		int a=4;
+		int b=6;
+//		System.out.println(~((-c)^1)==c);
 		System.out.println(new Solution().aplusb(a, b));
 	}
+	/**
+	 * 
+	 * @param a
+	 * @param b
+	 * @return
+	 * @note
+	 * 若c<0，则~(c^1)=|c|
+	 */
 	public int aplusb(int a, int b) {
 		if((a>>31&1)==1&&(b>>31&1)==1) {
 			//均为负数
 			System.out.println("a<0,b<0");
-			int result=(a<< 1)|(b<<1);
+			int absA=~(a^1);
+			int absB=~(b^1);
+			System.out.println(absA+" "+absB);
+			int result=(absA|absB);
 			System.out.println("result="+result);
-			return (result>>1);
+			return (result);
 		}else if((a>>31&1)==1&&(b>>31&1)==0) {
 			//a负b正
 			System.out.println("a>0,b<0");
